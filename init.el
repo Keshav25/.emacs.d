edfae32a75459b839c7c18f5ebbcdcd62a053915
Keshav25 <keshavitalia0@gmail.com>
2021-08-22 23:35:15 -0400

    Add files via upload

--- init.el
+++ init.el
@@ -1,3 +1,44 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
 (require 'eaf)
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0568a5426239e65aab5e7c48fa1abde81130a87ddf7f942613bf5e13bf79686b" "d6da24347c813d1635a217d396cf1e3be26484fd4d05be153f3bd2b293d2a0b5" "e2c926ced58e48afc87f4415af9b7f7b58e62ec792659fcb626e8cba674d2065" "e6f3a4a582ffb5de0471c9b640a5f0212ccf258a987ba421ae2659f1eaa39b09" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "8146edab0de2007a99a2361041015331af706e7907de9d6a330a3493a541e5a6" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "3d54650e34fa27561eb81fc3ceed504970cc553cfd37f46e8a80ec32254a3ec3" "846b3dc12d774794861d81d7d2dcdb9645f82423565bfb4dad01204fa322dbd5" "850bb46cc41d8a28669f78b98db04a46053eca663db71a001b40288a9b36796c" "1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "b0e446b48d03c5053af28908168262c3e5335dcad3317215d9fdeb8bac5bacf9" "e19ac4ef0f028f503b1ccafa7c337021834ce0d1a2bca03fcebc1ef635776bea" "4133d2d6553fe5af2ce3f24b7267af475b5e839069ba0e5c80416aa28913e89a" "6f4421bf31387397f6710b6f6381c448d1a71944d9e9da4e0057b3fe5d6f2fad" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "9b54ba84f245a59af31f90bc78ed1240fca2f5a93f667ed54bbf6c6d71f664ac" "cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa" "0d01e1e300fcafa34ba35d5cf0a21b3b23bc4053d388e352ae6a901994597ab1" "f490984d405f1a97418a92f478218b8e4bcc188cf353e5dd5d5acd2f8efd0790" "2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15" "6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "b5803dfb0e4b6b71f309606587dd88651efe0972a5be16ece6a958b197caeed8" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "9f9fc38446c384a4e909b7220d15bf0c152849ef42f5b1b97356448612c77953" "c5ded9320a346146bbc2ead692f0c63be512747963257f18cc8518c5254b7bf5" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" "da53441eb1a2a6c50217ee685a850c259e9974a8fa60e899d393040b4b8cc922" "35c096aa0975d104688a9e59e28860f5af6bb4459fd692ed47557727848e6dfe" "97db542a8a1731ef44b60bc97406c1eb7ed4528b0d7296997cbb53969df852d6" "28a104f642d09d3e5c62ce3464ea2c143b9130167282ea97ddcc3607b381823f" default))
+ '(package-selected-packages
+   '(evil-org evil-surround evil-goggles ace-window org-beautify-theme modus-themes solaire-mode org-download org-roam helm writeroom-mode epc deferred emacs-deferred ctable emacs-epc s\.el ranger olivetti yasnippet-snippets which-key use-package tron-legacy-theme switch-window swiper spaceline poet-theme peep-dired org-journal org-bullets meghanada magit ido-vertical-mode htmlize general gcmh evil-collection doom-themes doom-modeline dired-open diminish avy auto-package-update async all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(evil-goggles-change-face ((t (:inherit diff-removed))))
+ '(evil-goggles-delete-face ((t (:inherit diff-removed))))
+ '(evil-goggles-paste-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-add-face ((t (:inherit diff-added))))
+ '(evil-goggles-undo-redo-change-face ((t (:inherit diff-changed))))
+ '(evil-goggles-undo-redo-remove-face ((t (:inherit diff-removed))))
+ '(evil-goggles-yank-face ((t (:inherit diff-changed))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana" :height 1.5 :underline nil))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#EDE7dd" :font "Verdana"))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))) t)
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
