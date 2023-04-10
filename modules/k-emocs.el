683b3340d566c10d745bdd2651594f68bee7892d
keshav25I <keshavitalia0@gmail.com>
2023-04-10 16:22:36 -0400

    added new keybindings for embark commands and initial configuration

--- modules/k-emocs.el
+++ modules/k-emocs.el
@@ -38,8 +38,21 @@
 
 (leaf embark
   :ensure t
-  :bind (("C-c h" . embark-act)
+  :bind (("C-c h a" . embark-act)
+		 ("C-c h d" . embark-dwim)
+		 ("C-c h b" . embark-bindings)
 		 (:evil-normal-state-map
-		  ("zd" . embark-act))))
+		  ("zd" . embark-act)))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command)
+  (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+			   '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+				 nil
+				 (window-parameters (mode-line-format . none)))))
+
 
 (provide 'k-emocs)
