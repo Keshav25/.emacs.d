03295117b8dc4feb2e55eb75961679c0728d52dc
keshav25I <keshavitalia0@gmail.com>
2023-04-11 15:03:53 -0400

    added helpful

--- modules/k-misc.el
+++ modules/k-misc.el
@@ -1,8 +1,8 @@
 ;; User Information
 (setq user-full-name "Keshav Italia"
-      user-mail-address "keshavitalia0@gmail.com")
+	  user-mail-address "keshavitalia0@gmail.com")
 
-; Async
+										; Async
 (leaf async
   :ensure t
   :init
@@ -16,5 +16,15 @@
   :ensure t
   :config
   (gcmh-mode 1))
+
+(leaf helpful
+  :ensure t
+  :bind
+  ("C-h f" . 'helpful-callable)
+  ("C-h v" . 'helpful-variable)
+  ("C-h k" . 'helpful-key)
+  ("C-h x" . 'helpful-command)
+  ("C-h RET" . 'helpful-at-point)
+  ("C-h F" . 'helpful-function))
 
 (provide 'k-misc)
