b4a358e1d77ac52c8fb1e4e200e10ecd82c7c0fa
Keshav Italia <keshavitalia0@gmail.com>
2023-04-13 03:27:10 -0400

    added elisp-demos and more keybindings for help

--- modules/k-misc.el
+++ modules/k-misc.el
@@ -24,7 +24,19 @@
   ("C-h v" . 'helpful-variable)
   ("C-h k" . 'helpful-key)
   ("C-h x" . 'helpful-command)
+  ("C-h o" . 'helpful-symbol)
   ("C-h RET" . 'helpful-at-point)
-  ("C-h F" . 'helpful-function))
+  ("C-h F" . 'helpful-function)
+  ;; not helpful but might aswell
+  ("C-h K" . 'describe-keymap)
+  (:helpful-mode-map
+   ([remap revert-buffer] . 'helpful-update)))
+
+(leaf elisp-demos
+  :ensure t
+  :after (helpful)
+  :require t
+  :advice
+  (:after helpful-update elisp-demos-advice-helpful-update))
 
 (provide 'k-misc)
