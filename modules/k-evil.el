283e6c94ebce7a4ef5f7142e6b17e9aab3505aa1
keshav25I <keshavitalia0@gmail.com>
2023-04-17 22:51:28 -0400

    removed rebinding of p, P, and C-p in evil-mode for package.el funcs

--- modules/k-evil.el
+++ modules/k-evil.el
@@ -20,6 +20,7 @@
 	(search-backward (car consult--line-history)))
   (add-to-list 'evil-insert-state-modes 'text-mode)
   ;; https://github.com/minad/consult/issues/318
+<<<<<<< HEAD
   :bind ("C-M-u" . 'universal-argument)
   (:evil-insert-state-map
    ("C-a" . 'nil)
@@ -43,6 +44,23 @@
    ;;scroll the other window instead
    ("C-d" . 'scroll-other-window)
    ("C-u" . 'scroll-other-window-down))
+=======
+  :bind (:evil-normal-state-map
+		 ("n" . 'k-consult-line-search-forward)
+		 ("N" . 'k-consult-line-search-backward)
+		 ("F" . 'evil-find-char)
+		 ("ge" . 'evil-end-of-line)
+		 ("ga" . 'evil-first-non-blank)
+		 ("-" . 'dired)
+		 ;;unbind open and close folds
+		 ("zo" . 'nil)
+		 ("zc" . 'nil)
+		 ;;replace q
+		 ("q" . 'kill-buffer-and-window)
+		 ;;scroll the other window instead
+		 ("C-d" . 'scroll-other-window)
+		 ("C-u" . 'scroll-other-window-down))
+>>>>>>> fc724da (removed rebinding of p, P, and C-p in evil-mode for package.el funcs)
   (:evil-motion-state-map
    ("ge" . 'evil-end-of-line)
    ;;unbind a few emacs keys
