cbe17798e51d4d79b8e7b21cc70094924b06dea1
Keshav Italia <keshavitalia0@gmail.com>
2023-04-17 22:56:01 -0400

    added package treemacs, readded flymake, flymake-collections

--- modules/k-programming.el
+++ modules/k-programming.el
@@ -1,6 +1,19 @@
 (leaf eglot)
+
 (leaf flymake
-  (leaf flymake-collections))
-(leaf envrc)
+  :ensure t
+  :config
+  (leaf flymake-collections
+	:ensure t))
+
+(leaf envrc
+  :ensure t)
+
+(leaf treemacs
+  :ensure t
+  :setq
+  (treemacs-width . 25)
+  :hook (treemacs-mode-hook . '(text-scale-adjust -1)))
+
 
 (provide 'k-programming)
