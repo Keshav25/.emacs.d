c5085f616189a50618ea3ea8d53fc6946066a408
keshav25I <keshavitalia0@gmail.com>
2023-04-24 16:31:28 -0400

    remove alert function from init file

--- modules/k-evil.el
+++ modules/k-evil.el
@@ -6,7 +6,7 @@
   (evil-want-integration . t)
   (evil-want-keybinding . nil)
   (evil-want-fine-undo . t)
-  (evil-respect-visual-line-mode . t)
+  (evil-respect-visual-line-mode . f)
   (evil-want-C-u-scroll . t)
   (evil-want-vsplit-window-right . t)
   (evil-want-split-window-below . t)
