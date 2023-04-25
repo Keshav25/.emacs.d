3f91489e3da98fa0d5f4e302379254695961f9cb
Keshav Italia <keshavitalia0@gmail.com>
2023-04-25 18:14:41 -0400

    Initial dirvish setup

--- modules/k-dired.el
+++ modules/k-dired.el
@@ -1,9 +1,9 @@
-;; Dired
-(leaf all-the-icons-dired :ensure t
-  :hook ((dired-mode-hook . all-the-icons-dired-mode)))
+;; ;; Dired
+;; (leaf all-the-icons-dired :ensure t
+;;   :hook ((dired-mode-hook . all-the-icons-dired-mode)))
 
 (leaf dired-open :ensure t)
-(leaf peep-dired :ensure t)
+;; (leaf peep-dired :ensure t)
 (leaf dirvish
   :ensure t
   :config
@@ -12,7 +12,7 @@
 			  ("q" . 'dirvish-quit)))
 
 ;; Get file icons in dired
-(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+;; (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
 ;; With dired-open plugin, you can launch external programs for certain extensions
 ;; For example, I set all .png files to open in 'sxiv' and all .mp4 files to open in 'mpv'
 (setq dired-open-extensions '(("gif" . "sxiv")
