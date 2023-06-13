8b4364bce5b0a9919d1a18760e0eb50355764740
Keshav Italia <keshavitalia0@gmail.com>
2023-06-13 17:28:31 -0400

    formatting

--- modules/k-wm.el
+++ modules/k-wm.el
@@ -7,13 +7,13 @@
 
 ;; Window Divider Mode
 (setq window-divider-default-places t
-      window-divider-default-bottom-width 1
-      window-divider-default-right-width 1)
+	  window-divider-default-bottom-width 1
+	  window-divider-default-right-width 1)
 (window-divider-mode 1)
 
 ;; Split Thresholds
 (setq split-width-threshold 160
-      split-height-threshold nil)
+	  split-height-threshold nil)
 
 ;; Switch Window
 (leaf switch-window
@@ -24,24 +24,24 @@
   (switch-window-threshold . 2)
   (switch-window-shortcut-style . 'qwerty)
   (switch-window-qwerty-shortcuts .
-				  '("a" "s" "d" "f" "j" "k" "l"))
+								  '("a" "s" "d" "f" "j" "k" "l"))
   :bind
   ([remap other-window] . switch-window))
 
 ;; Split and Follow Functions
 (defun split-and-follow-horizontally ()
-	(interactive)
-	(split-window-below)
-	(balance-windows)
-	(other-window 1))
- (global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
+  (interactive)
+  (split-window-below)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
 
- (defun split-and-follow-vertically ()
-	(interactive)
-	(split-window-right)
-	(balance-windows)
-	(other-window 1))
- (global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
+(defun split-and-follow-vertically ()
+  (interactive)
+  (split-window-right)
+  (balance-windows)
+  (other-window 1))
+(global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
 
 ;; Toggle fullscreen
 (defun k-toggle-fullscreen ()
@@ -50,7 +50,7 @@
   (if (and winner-mode
 		   (equal (selected-window) (next-window)))
 	  (winner-undo)
-	    (delete-other-windows)))
+	(delete-other-windows)))
 
 (global-set-key (kbd "C-x 1") 'k-toggle-fullscreen)
 
@@ -81,26 +81,26 @@
   ;; Author: Jeff Dwork
   (interactive)
   (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  #'split-window-horizontally
-                #'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
+	  (let* ((this-win-buffer (window-buffer))
+			 (next-win-buffer (window-buffer (next-window)))
+			 (this-win-edges (window-edges (selected-window)))
+			 (next-win-edges (window-edges (next-window)))
+			 (this-win-2nd (not (and (<= (car this-win-edges)
+										 (car next-win-edges))
+									 (<= (cadr this-win-edges)
+										 (cadr next-win-edges)))))
+			 (splitter
+			  (if (= (car this-win-edges)
+					 (car (window-edges (next-window))))
+				  #'split-window-horizontally
+				#'split-window-vertically)))
+		(delete-other-windows)
+		(let ((first-win (selected-window)))
+		  (funcall splitter)
+		  (if this-win-2nd (other-window 1))
+		  (set-window-buffer (selected-window) this-win-buffer)
+		  (set-window-buffer (next-window) next-win-buffer)
+		  (select-window first-win)
+		  (if this-win-2nd (other-window 1))))))
 
 (provide 'k-wm)
