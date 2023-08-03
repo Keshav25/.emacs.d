cd2ae536e9714004fa0b0f1699e8b585b2ecc404
Keshav25 <keshavitalia0@gmail.com>
2023-08-03 18:30:38 -0400

    removed commented out eaf configuration as it has been moved to k-os

--- init.el
+++ init.el
@@ -80,39 +80,4 @@
 ;;    (vterm)
 ;;    (evil-emacs-state))
 
-;; ;; EAF
-;; (leaf eaf
-;;   :when (or islinux iswindows)
-;;   :load-path "~/.emacs.d/site-lisp/emacs-application-framework/"
-;;   :config
-;;   (require 'eaf)
-;;   (require 'eaf-browser)
-;;   (require 'eaf-evil)
-;;   (require 'eaf-all-the-icons)
-;;   (require 'eaf-2048)
-;;   (require 'eaf-airshare)
-;;   (require 'eaf-camera)
-;;   (require 'eaf-demo)
-;;   (require 'eaf-file-browser)
-;;   (require 'eaf-file-manager)
-;;   (require 'eaf-file-sender)
-;;   (require 'eaf-git)
-;;   (require 'eaf-image-viewer)
-;;   (require 'eaf-jupyter)
-;;   (require 'eaf-markdown-previewer)
-;;   (require 'eaf-mindmap)
-;;   (require 'eaf-music-player)
-;;   (require 'eaf-netease-cloud-music)
-;;   (require 'eaf-org-previewer)
-;;   (require 'eaf-pdf-viewer)
-;;   (require 'eaf-rss-reader)
-;;   (require 'eaf-system-monitor)
-;;   (require 'eaf-terminal)
-;;   (require 'eaf-video-player)
-;;   (require 'eaf-vue-demo)
-;;   (require 'eaf-vue-mindmap)
-;;   :setq
-;;   (eaf-browser-search-engines . '(("duckduckgo" . "https://duckduckgo.com/?q=%s")))
-;;   (eaf-browser-default-search-engine . "duckduckgo"))
-
 (alert "emacs has loaded")
