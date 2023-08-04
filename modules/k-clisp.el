0bbbf060e9e427253a531400ce179a999d4fc4ab
Keshav25 <keshavitalia0@gmail.com>
2023-08-04 14:00:59 -0400

    common lisp module

---
+++ modules/k-clisp.el
@@ -0,0 +1,30 @@
+(leaf slime
+  :ensure t)
+
+(leaf ac-slime
+  :ensure t)
+
+(leaf slime-company
+  :ensure t)
+
+(leaf slime-repl-ansi-color
+  :ensure t)
+
+(leaf paredit
+  :ensure t)
+
+(leaf separedit
+  :ensure t)
+
+(leaf evil-paredit
+  :after evil
+  :ensure t)
+
+(leaf evil-paredit
+  :ensure t)
+
+(leaf paredit-everywhere
+  :ensure t)
+
+(leaf sly-quicklisp
+  :ensure t)
