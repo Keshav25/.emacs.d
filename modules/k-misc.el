a3886bae4a3bca34ebd19b98e96740bb0c39f479
keshav25I <keshavitalia0@gmail.com>
2023-08-05 16:00:32 -0400

    detached and inform

--- modules/k-misc.el
+++ modules/k-misc.el
@@ -2,11 +2,26 @@
 (setq user-full-name "Keshav Italia"
 	  user-mail-address "keshavitalia0@gmail.com")
 
-										; Async
+;; Async
 (leaf async
   :ensure t
   :init
   (dired-async-mode 1))
+
+;; (use-package detached
+;;   :ensure t
+;;   ;; :ensure-system-package "dtach"
+;;   :init
+;;   (detached-init)
+;;   :bind (;; Replace `async-shell-command' with `detached-shell-command'
+;;          ([remap async-shell-command] . detached-shell-command)
+;;          ;; Replace `compile' with `detached-compile'
+;;          ([remap compile] . detached-compile)
+;;          ([remap recompile] . detached-compile-recompile)
+;;          ;; Replace built in completion of sessions with `consult'
+;;          ([remap detached-open-session] . detached-consult-session))
+;;   :custom ((detached-show-output-on-attach t)
+;;            (detached-terminal-data-command system-type)))
 
 ;; keep emacs clean
 (leaf no-littering :ensure t)
@@ -32,11 +47,17 @@
   (:helpful-mode-map
    ([remap revert-buffer] . 'helpful-update)))
 
+(leaf inform
+  :ensure t)
+
+
 (leaf elisp-demos
   :ensure t
   :after (helpful)
   :require t
   :advice
   (:after helpful-update elisp-demos-advice-helpful-update))
+
+(setq browse-url-browser-function 'browse-url-xdg-open)
 
 (provide 'k-misc)
