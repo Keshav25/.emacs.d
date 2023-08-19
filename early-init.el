9a4bcaf0a04f7184e7ac60ddc16110daa660f0dc
Keshav25 <keshavitalia0@gmail.com>
2023-08-19 18:22:49 -0400

    testing autofmt

--- early-init.el
+++ early-init.el
@@ -4,8 +4,9 @@
   (server-start))
 
 ;; Garbage Collection
-(setq gc-cons-threshold most-positive-fixnum
-	  gc-cons-percentage 0.6)
+(setq
+ gc-cons-threshold most-positive-fixnum
+ gc-cons-percentage 0.6)
 
 ;; startup message
 ;; (add-hook 'emacs-startup-hook
@@ -17,9 +18,10 @@
 ;; 					 gcs-done)))
 
 ;; Native Comp Errors
-(setq comp-deferred-compilation nil
-	  native-comp-deferred-compilation nil
-	  comp-async-report-warnings-errors nil)
+(setq
+ comp-deferred-compilation nil
+ native-comp-jit-compilation nil
+ comp-async-report-warnings-errors nil)
 
 (setq load-prefer-newer noninteractive)
 
@@ -27,27 +29,42 @@
 (setq package-native-compile t)
 
 ;; backup files
-(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+(setq backup-directory-alist
+      `(("."
+         .
+         ,(expand-file-name "tmp/backups/" user-emacs-directory))))
 
-(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
-(setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
-	  auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
+(make-directory (expand-file-name "tmp/auto-saves/"
+                                  user-emacs-directory)
+                t)
+(setq
+ auto-save-list-file-prefix
+ (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
+ auto-save-file-name-transforms
+ `((".*"
+    ,(expand-file-name "tmp/auto-saves/" user-emacs-directory)
+    t)))
 
-(setq projectile-known-projects-file (expand-file-name "tmp/projectile-bookmarks.eld" user-emacs-directory)
-	  lsp-session-file (expand-file-name "tmp/.lsp-session-v1" user-emacs-directory))
+(setq
+ projectile-known-projects-file
+ (expand-file-name "tmp/projectile-bookmarks.eld"
+                   user-emacs-directory)
+ lsp-session-file (expand-file-name "tmp/.lsp-session-v1" user-emacs-directory))
 
-(setq backup-by-copying t      ; don't clobber symlinks
-	  delete-old-versions t
-	  kept-new-versions 6
-	  kept-old-versions 2
-	  version-control t)       ; use versioned backups
+(setq
+ backup-by-copying t ; don't clobber symlinks
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t) ; use versioned backups
 
 (message "Deleting old backup files...")
 (let ((week (* 60 60 24 7))
       (current (float-time (current-time))))
   (dolist (file (directory-files temporary-file-directory t))
     (when (and (backup-file-name-p file)
-               (> (- current (float-time (fifth (file-attributes file))))
+               (> (- current
+                     (float-time (fifth (file-attributes file))))
                   (* 4 week)))
       (message "%s" file)
       (delete-file file))))
@@ -60,8 +77,10 @@
 ;; Determine OS
 (require 'subr-x)
 (defconst islinux (eq system-type 'gnu/linux))
-(defconst iswindows (eq system-type '(cygwin windows-nt ms-dos)))
-(defconst istermux (string-suffix-p "Android" (string-trim (shell-command-to-string "uname -a"))))
+(defconst iswindows (eq system-type 'windows-nt))
+(defconst istermux
+  (string-suffix-p
+   "Android" (string-trim (shell-command-to-string "uname -a"))))
 (defconst isguix nil)
 
 ;; Get Home Directory if Windows
@@ -70,12 +89,12 @@
   (setq abbreviated-home-dir nil))
 
 ;; Determine if Native Comp
-(defconst isnativecomp (if (fboundp 'native-comp-available-p)
-						   (native-comp-available-p)))
+(defconst isnativecomp
+  (if (fboundp 'native-comp-available-p)
+      (native-comp-available-p)))
 
 ;; Determine if EXWM should be enabled
-(setq isexwm (and (not istermux)
-				  (eq window-system 'x)))
+(setq isexwm (and (not istermux) (eq window-system 'x)))
 
 ;; Language Environment
 (set-language-environment "UTF-8")
@@ -94,76 +113,83 @@
 (advice-add #'display-startup-echo-area-message :override #'ignore)
 
 ;; Bidirectional Text
-(setq-default bidi-display-reordering 'left-to-right
-			  bidi-paragraph-direction 'left-to-right)
+(setq-default
+ bidi-display-reordering 'left-to-right
+ bidi-paragraph-direction 'left-to-right)
 (setq bidi-inhibit-bpa t)
 
 
 ;; Keep .emacs.d clean
-(setq user-emacs-directory (expand-file-name "~/.emacs.d")
-	  url-history-file (expand-file-name "url/history" user-emacs-directory))
+(setq
+ user-emacs-directory (expand-file-name "~/.emacs.d")
+ url-history-file (expand-file-name "url/history" user-emacs-directory))
 
 
 (setq custom-file
-	  (if (boundp 'server-socket-dir)
-		  (expand-file-name "~/.emacs.d/custom.el" server-socket-dir)
-		(expand-file-name (format "emacs-custom-$s.el" (user-uid)) temporary-file-directory)))
+      (if (boundp 'server-socket-dir)
+          (expand-file-name "~/.emacs.d/custom.el" server-socket-dir)
+        (expand-file-name (format "emacs-custom-$s.el" (user-uid))
+                          temporary-file-directory)))
 
 
 ;; Visual Settings
 (setq frame-resize-pixelwise t)
 (setq window-resize-pixelwise nil)
-(setq indicate-buffer-boundaries nil
-	  indicate-empty-lines nil)
+(setq
+ indicate-buffer-boundaries nil
+ indicate-empty-lines nil)
 
 ;; Disable Graphical Menus
 (setq-default default-frame-alist
-			  '((tool-bar-lines . 0)
-				(menu-bar-lines . 0)
-				;; (undecorated . t)
-				(vertical-scroll-bars . nil)
-				(horizontal-scroll-bars . nil)))
+              '((tool-bar-lines . 0)
+                (menu-bar-lines . 0)
+                ;; (undecorated . t)
+                (vertical-scroll-bars . nil)
+                (horizontal-scroll-bars . nil)))
 ;; (push '(menu-bar-lines . 0)   default-frame-alist)
 ;; (push '(tool-bar-lines . 0)   default-frame-alist)
 ;; (push '(vertical-scroll-bars) default-frame-alist)
 
 ;; (setq menu-bar-mode nil
-	  ;; tool-bar-mode nil
-	  ;; scroll-bar-mode nil)
+;; tool-bar-mode nil
+;; scroll-bar-mode nil)
 
 (setq initial-frame-alist
-	  '((right-divider-width . 24)
-		(internal-border-width. 24)))
+      '((right-divider-width . 24) (internal-border-width. 24)))
 
 ;; Recursive Minibuffers
 (setq enable-recursive-minibuffers t)
 
 
 ;; Transparency
-(if (< emacs-major-version 29)
-	(progn
-	  (set-frame-parameter (selected-frame) 'alpha `(95,95))
-	  (add-to-list 'default-frame-alist `(alpha . (95, 95)))
+(if (or (< emacs-major-version 29)
+		iswindows)
+    (progn
+      (set-frame-parameter (selected-frame) 'alpha `(75,75))
+      (add-to-list 'default-frame-alist `(alpha . (75, 75)))
 
-	  (defun turn-transparency-off ()
-		(interactive)
-		(set-frame-parameter (selected-frame) 'alpha `(100, 100)))
+      (defun turn-transparency-off ()
+        (interactive)
+        (set-frame-parameter (selected-frame) 'alpha `(100, 100)))
 
-	  (defun turn-transparency-on ()
-		(interactive)
-		(set-frame-parameter (selected-frame) 'alpha `(95, 95))))
+      (defun turn-transparency-on ()
+        (interactive)
+        (set-frame-parameter (selected-frame) 'alpha `(75, 75))))
   (progn
-	;; for emacs => 29
-	(set-frame-parameter (selected-frame) 'alpha-background 75)
-	(add-to-list 'default-frame-alist '(alpha-background . 75))
+    ;; for emacs => 29
+    (set-frame-parameter (selected-frame) 'alpha-background 75)
+    (add-to-list 'default-frame-alist '(alpha-background . 75))
 
-	(defun k/toggle-transparency ()
-	  "toggle transparency for emacs-major-version > 29"
-	  (interactive)
-	  (let ((alpha-transparency 75))
-		(pcase (frame-parameter nil 'alpha-background)
-		  (alpha-transparency (set-frame-parameter nil 'alpha-background 100))
-		  (t (set-frame-parameter nil 'alpha-background alpha-transparency)))))))
+    (defun k/toggle-transparency ()
+      "toggle transparency for emacs-major-version > 29"
+      (interactive)
+      (let ((alpha-transparency 75))
+        (pcase (frame-parameter nil 'alpha-background)
+          (alpha-transparency
+           (set-frame-parameter nil 'alpha-background 100))
+          (t
+           (set-frame-parameter
+            nil 'alpha-background alpha-transparency)))))))
 
 
 (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
@@ -180,11 +206,8 @@
 ;; General Text Editing Preferences
 (show-paren-mode)
 (global-prettify-symbols-mode t)
-(setq electric-pair-pairs '((?\{ . ?\})
-							(?\( . ?\))
-							(?\[ . ?\])
-							(?\" . ?\")
-							))
+(setq electric-pair-pairs
+      '((?\{ . ?\}) (?\( . ?\)) (?\[ . ?\]) (?\" . ?\")))
 (electric-pair-mode t)
 
 ;; Display Time
@@ -195,14 +218,15 @@
 (setq-default cursor-in-non-selected-windows nil)
 (setq highlight-nonselected-windows nil)
 (setq fast-but-imprecise-scrolling t)
-(setq hscroll-margin 2
-	  hscroll-step 1
-	  scroll-conservatively 101
-	  scroll-margin 0
-	  scroll-preserve-screen-position t
-	  auto-window-vscroll nil
-	  mouse-wheel-scroll-amount '(2 ((shift) . hscroll))
-	  mouse-wheel-scroll-amount-horizon 2)
+(setq
+ hscroll-margin 2
+ hscroll-step 1
+ scroll-conservatively 101
+ scroll-margin 0
+ scroll-preserve-screen-position t
+ auto-window-vscroll nil
+ mouse-wheel-scroll-amount '(2 ((shift) . hscroll))
+ mouse-wheel-scroll-amount-horizon 2)
 (setq x-stretch-cursor nil)
 
 (provide 'early-init)
