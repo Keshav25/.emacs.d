5184fc06fe9984948bfee05baff68b1f02fc3343
Keshav Italia <keshavitalia0@gmail.com>
2023-08-31 12:56:50 -0400

    lambda line

--- modules/k-themes.el
+++ modules/k-themes.el
@@ -54,7 +54,43 @@
 	:ensure t))
 
 (leaf all-the-icons-nerd-fonts
-			 :quelpa
-			 (all-the-icons-nerd-fonts :fetcher github :repo "mohkale/all-the-icons-nerd-fonts"))
+  :quelpa
+  (all-the-icons-nerd-fonts :fetcher github :repo "mohkale/all-the-icons-nerd-fonts"))
+
+(leaf lambda-line
+  :quelpa (lambda-line :fetcher github :repo "lambda-emacs/lambda-line") 
+  :custom
+  (lambda-line-icon-time . t) ;; requires ClockFace font (see below)
+  (lambda-line-clockface-update-fontset . "ClockFaceRect") ;; set clock icon
+  (lambda-line-position . 'top) ;; Set position of status-line 
+  (lambda-line-abbrev . t) ;; abbreviate major modes
+  (lambda-line-hspace . "  ")  ;; add some cushion
+  (lambda-line-prefix . t) ;; use a prefix symbol
+  (lambda-line-prefix-padding . nil) ;; no extra space for prefix 
+  (lambda-line-status-invert . nil)  ;; no invert colors
+  (lambda-line-gui-ro-symbol .  " ⨂") ;; symbols
+  (lambda-line-gui-mod-symbol . " ⬤") 
+  (lambda-line-gui-rw-symbol .  " ◯") 
+  (lambda-line-space-top . +.50)  ;; padding on top and bottom of line
+  (lambda-line-space-bottom . -.50)
+  (lambda-line-symbol-position . 0.1) ;; adjust the vertical placement of symbol
+  :config
+  ;; activate lambda-line 
+  (lambda-line-mode) 
+  ;; set divider line in footer
+  (when (eq lambda-line-position 'top)
+	(setq-default mode-line-format (list "%_"))
+	(setq mode-line-format (list "%_"))))
+
+;; (customize-set-variable 'flymake-mode-line-counter-format '("" flymake-mode-line-error-counter flymake-mode-line-warning-counter flymake-mode-line-note-counter ""))
+;; (customize-set-variable 'flymake-mode-line-format '(" " flymake-mode-line-exception flymake-mode-line-counters))
+
+;;(use-package fontset
+;;			 :straight (:type built-in) ;; only include this if you use straight
+;;			 :config
+;;			 ;; Use symbola for proper unicode
+;;			 (when (member "Symbola" (font-family-list))
+;;			   (set-fontset-font
+;;				t 'symbol "Symbola" nil)))
 
 (provide 'k-themes)
