4945f218def58dd10120693187ce041a7c6a0054
Keshav25 <keshavitalia0@gmail.com>
2023-09-01 23:18:35 -0400

    eshell and eshell-icat blocks

--- modules/k-eshell.el
+++ modules/k-eshell.el
@@ -1,24 +1,34 @@
-;; Eshell mycat command
-;; To know if I need to print as image or I need to default to the default eshell/cat
-(defun my-is-imagep (filename)
-  (let ((extension (file-name-extension filename))
-		(image-extensions '("png" "jpg" "bmp")))
-	(member extension image-extensions)))
+(leaf eshell
+  :require t
+  :require 'em-smart
+  :custom
+  (eshell-where-to-jump . 'begin)
+  (eshell-review-quick-commands . nil)
+  (eshell-smart-space-goes-to-end . t))
 
-;; Creates a space with display properties. Feel free to change `eshell/println` to `insert` and use it in a normal emacs buffer, it will inline the path given in `file`.
-(defun my-print-image-eshell (file)
-  (eshell/printnl (propertize " " 'display (create-image file))))
+(leaf eshell-icat
+  :config
+  ;; Eshell mycat command
+  ;; To know if I need to print as image or I need to default to the default eshell/cat
+  (defun my-is-imagep (filename)
+	(let ((extension (file-name-extension filename))
+		  (image-extensions '("png" "jpg" "bmp")))
+	  (member extension image-extensions)))
+
+  ;; Creates a space with display properties. Feel free to change `eshell/println` to `insert` and use it in a normal emacs buffer, it will inline the path given in `file`.
+  (defun my-print-image-eshell (file)
+	(eshell/printnl (propertize " " 'display (create-image file))))
 
 
-;; If image, use `my-print-image-eshell`. Otherwise, just use `eshell/cat`.
-(defun eshell/mycat (&rest args)
-  (interactive)
-  (mapc (lambda (arg)
-		  (if (my-is-imagep arg)
-			  (my-print-image-eshell arg)
-			(eshell/cat arg)))
-		(-flatten args))
-  nil)
+  ;; If image, use `my-print-image-eshell`. Otherwise, just use `eshell/cat`.
+  (defun eshell/mycat (&rest args)
+	(interactive)
+	(mapc (lambda (arg)
+			(if (my-is-imagep arg)
+				(my-print-image-eshell arg)
+			  (eshell/cat arg)))
+		  (-flatten args))
+	nil))
 
 (leaf esh-help
   :ensure t
