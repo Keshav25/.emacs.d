179ff7425216b00afab0a9a2cf536c1ad11172b6
Keshav25 <keshavitalia0@gmail.com>
2023-09-01 23:20:15 -0400

    org-agenda-files directories

--- modules/k-org.el
+++ modules/k-org.el
@@ -52,7 +52,7 @@
 (leaf org-agenda
   :bind (("C-c a" . org-agenda))
   :config
-  (custom-set-variables org-agenda-files "~/org/agenda/")
+  (custom-set-variables org-agenda-files (directory-files-recursively "~/org/agenda/" "~/Documents/Journal"))
   :hook ((org-agenda-finalize . org-modern-agenda)
 		 (org-agenda-finalize . hl-line-mode)))
 
