9fdedd098699f7bb1a27ac6ed2e80d6840ef1449
Keshav25 <keshavitalia0@gmail.com>
2024-03-20 02:20:03 -0400

    allow org-refile to refile to top level of file

--- modules/k-org.el
+++ modules/k-org.el
@@ -23,6 +23,7 @@
   (org-agenda-timegrid-use-ampm . nil)
   (org-return-follows-link . t)
   (org-mouse-1-follows-link . t)
+  (org-refile-use-outline-path . 'file)
   ;; (org-agenda-time-grid . ((daily today require-timed)
   ;; (600 630 700 730 800 830 900 930 1000 1030 1100 1130 1200 1230 1300 1330 1400 1430 1500 1530 1600 1630 1700 1730 1800 1830 2000 2030 2100 2130 2200 2230 2300 2330)
   ;; " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"))
