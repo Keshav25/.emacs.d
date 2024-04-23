dca1d21076c2fe3a7da5b667e5a0fc21a0a57ebd
Keshav Italia <keshavitalia0@gmail.com>
2024-04-23 12:59:44 -0400

    goodbye nerd fonts and lambda lines

--- modules/k-themes.el
+++ modules/k-themes.el
@@ -54,50 +54,50 @@
   (leaf all-the-icons-completion
 	:ensure t))
 
-(leaf nerd-icons
-  :ensure t
-  :require t
-  :custom
-  (nerd-icons-font-family . "JetbrainsMono Nerd Font"))
+;; (leaf nerd-icons
+;;   :ensure t
+;;   :require t
+;;   :custom
+;;   (nerd-icons-font-family . "JetbrainsMono Nerd Font"))
 
-(leaf all-the-icons-nerd-fonts
-  :require t
-  :quelpa
-  (all-the-icons-nerd-fonts :fetcher github :repo "mohkale/all-the-icons-nerd-fonts")
-  :require t
-  :after (all-the-icons)
-  :config
-  (all-the-icons-nerd-fonts-prefer))
+;; (leaf all-the-icons-nerd-fonts
+;;   :require t
+;;   :quelpa
+;;   (all-the-icons-nerd-fonts :fetcher github :repo "mohkale/all-the-icons-nerd-fonts")
+;;   :require t
+;;   :after (all-the-icons)
+;;   :config
+;;   (all-the-icons-nerd-fonts-prefer))
 
-(leaf nerd-fonts
-  :require t
-  :quelpa
-  (nerd-fonts :fetcher github :repo "twlz0ne/nerd-fonts.el"))
+;; (leaf nerd-fonts
+;;   :require t
+;;   :quelpa
+;;   (nerd-fonts :fetcher github :repo "twlz0ne/nerd-fonts.el"))
 
-(leaf lambda-line
-  :quelpa (lambda-line :fetcher github :repo "lambda-emacs/lambda-line") 
-  :custom
-  (lambda-line-icon-time . t) ;; requires ClockFace font (see below)
-  (lambda-line-clockface-update-fontset . "ClockFaceRect") ;; set clock icon
-  (lambda-line-position . 'top) ;; Set position of status-line 
-  (lambda-line-abbrev . t) ;; abbreviate major modes
-  (lambda-line-hspace . "  ")  ;; add some cushion
-  (lambda-line-prefix . t) ;; use a prefix symbol
-  (lambda-line-prefix-padding . nil) ;; no extra space for prefix 
-  (lambda-line-status-invert . nil)  ;; no invert colors
-  (lambda-line-gui-ro-symbol .  " ⨂") ;; symbols
-  (lambda-line-gui-mod-symbol . " ⬤") 
-  (lambda-line-gui-rw-symbol .  " ◯") 
-  (lambda-line-space-top . +.50)  ;; padding on top and bottom of line
-  (lambda-line-space-bottom . -.50)
-  (lambda-line-symbol-position . 0.1) ;; adjust the vertical placement of symbol
-  :config
-  ;; activate lambda-line 
-  (lambda-line-mode) 
-  ;; set divider line in footer
-  (when (eq lambda-line-position 'top)
-	(setq-default mode-line-format (list "%_"))
-	(setq mode-line-format (list "%_"))))
+;; (leaf lambda-line
+;;   :quelpa (lambda-line :fetcher github :repo "lambda-emacs/lambda-line") 
+;;   :custom
+;;   (lambda-line-icon-time . t) ;; requires ClockFace font (see below)
+;;   (lambda-line-clockface-update-fontset . "ClockFaceRect") ;; set clock icon
+;;   (lambda-line-position . 'top) ;; Set position of status-line 
+;;   (lambda-line-abbrev . t) ;; abbreviate major modes
+;;   (lambda-line-hspace . "  ")  ;; add some cushion
+;;   (lambda-line-prefix . t) ;; use a prefix symbol
+;;   (lambda-line-prefix-padding . nil) ;; no extra space for prefix 
+;;   (lambda-line-status-invert . nil)  ;; no invert colors
+;;   (lambda-line-gui-ro-symbol .  " ⨂") ;; symbols
+;;   (lambda-line-gui-mod-symbol . " ⬤") 
+;;   (lambda-line-gui-rw-symbol .  " ◯") 
+;;   (lambda-line-space-top . +.50)  ;; padding on top and bottom of line
+;;   (lambda-line-space-bottom . -.50)
+;;   (lambda-line-symbol-position . 0.1) ;; adjust the vertical placement of symbol
+;;   :config
+;;   ;; activate lambda-line 
+;;   (lambda-line-mode) 
+;;   ;; set divider line in footer
+;;   (when (eq lambda-line-position 'top)
+;; 	(setq-default mode-line-format (list "%_"))
+;; 	(setq mode-line-format (list "%_"))))
 
 ;; (customize-set-variable 'flymake-mode-line-counter-format '("" flymake-mode-line-error-counter flymake-mode-line-warning-counter flymake-mode-line-note-counter ""))
 ;; (customize-set-variable 'flymake-mode-line-format '(" " flymake-mode-line-exception flymake-mode-line-counters))
