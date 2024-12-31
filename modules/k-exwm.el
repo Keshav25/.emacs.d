c9f2ce3eeb3d8f3eb2ee8102ea7e3809ac73b84f
Keshav25 <keshavitalia0@gmail.com>
2024-12-31 01:21:19 -0500

    M-t for execute-extended-command in exwm buffers

--- modules/k-exwm.el
+++ modules/k-exwm.el
@@ -65,6 +65,7 @@
 		(,(kbd "s-'") . fhd/toggle-exwm-input-line-mode-passthrough)
         (,(kbd "s-c") . kill-buffer-and-window)
 		(,(kbd "C-`") . popper-toggle)
+		(,(kbd "M-t") . execute-extended-command)
         ,@(mapcar (lambda (i)
                     `(,(kbd (format "s-%d" i)) .
                       (lambda ()
