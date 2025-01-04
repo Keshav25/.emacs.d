a7b392ce0bd0d70701a665200d17f602bfb6c4e0
Keshav25 <keshavitalia0@gmail.com>
2025-01-03 22:12:23 -0500

    straight and use-package

--- modules/k-leaf.el
+++ modules/k-leaf.el
@@ -13,7 +13,25 @@
   (package-initialize)
   (unless (package-installed-p 'leaf)
     (package-refresh-contents)
-    (package-install 'leaf)))
+    (package-install 'leaf))
+
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+		 (expand-file-name
+          "straight/repos/straight.el/bootstrap.el"
+          (or (bound-and-true-p straight-base-dir)
+              user-emacs-directory)))
+		(bootstrap-version 7))
+	(unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+           'silent 'inhibit-cookies)
+		(goto-char (point-max))
+		(eval-print-last-sexp)))
+	(load bootstrap-file nil 'nomessage)))
+
+(straight-use-package 'use-package)
 
 ;; Auto Package Update
 ;; (leaf auto-package-update
