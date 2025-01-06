a55ca4a0b3eb5da6e7de77ec24355458d9d5188f
Keshav25 <keshavitalia0@gmail.com>
2025-01-06 00:46:35 -0500

    fixed k/fill-or-unfill

--- modules/k-bindings.el
+++ modules/k-bindings.el
@@ -152,13 +152,13 @@
 	"Like `fill-paragraph', but unfill if used twice."
 	(interactive)
 	(let ((fill-column
-           (if (eq last-command 'endless/fill-or-unfill)
+           (if (eq last-command 'k/fill-or-unfill)
                (progn (setq this-command nil)
                       (point-max))
 			 fill-column)))
       (call-interactively #'fill-paragraph)))
 
   (global-set-key [remap fill-paragraph]
-                  #'endless/fill-or-unfill))
+                  #'k/fill-or-unfill))
 
 (provide 'k-bindings)
