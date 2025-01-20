adaa080d0c675ccc07746063357ab3d0259bf370
Keshav25 <keshavitalia0@gmail.com>
2025-01-20 03:18:09 -0500

    replace windmove with ace-window an transpose-frame leafs

--- modules/k-wm.el
+++ modules/k-wm.el
@@ -93,7 +93,31 @@
 
 
 (leaf tabspaces :ensure t)
-(leaf windmove :ensure t)
+
+(leaf ace-window
+  :ensure t
+  :config
+  (setq aw-keys '(?i ?s ?r ?t ?g ?p ?n ?e ?o)
+		aw-background nil)
+  (defvar aw-dispatch-alist
+	'((?x aw-delete-window "Delete Window")
+	  (?m aw-swap-window "Swap Windows")
+	  (?M aw-move-window "Move Window")
+	  (?c aw-copy-window "Copy Window")
+	  (?j aw-switch-buffer-in-window "Select Buffer")
+	  (?n aw-flip-window)
+	  (?u aw-switch-buffer-other-window "Switch Buffer Other Window")
+	  (?c aw-split-window-fair "Split Fair Window")
+	  (?v aw-split-window-vert "Split Vert Window")
+	  (?b aw-split-window-horz "Split Horz Window")
+	  (?o delete-other-windows "Delete Other Windows")
+	  (?? aw-show-dispatch-help))
+	"List of actions for `aw-dispatch-default'.")
+  :bind
+  ("C-x o" . ace-window))
+
+(leaf transpose-frame
+  :ensure t)
 
 (leaf centered-window
   :ensure t)
