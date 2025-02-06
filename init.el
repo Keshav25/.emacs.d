3f3dd962b792ba2dbd90f96fab93323010f70637
Keshav25 <keshavitalia0@gmail.com>
2025-02-06 14:24:51 -0500

    changed default font to JetbrainsMono-9

--- init.el
+++ init.el
@@ -150,7 +150,7 @@
                 ;; (undecorated . t)
                 (vertical-scroll-bars . nil)
                 (horizontal-scroll-bars . nil)
-				;; (font . "JetBrainsMono-12:weigth=regular:width=normal")
+				(font . "JetBrainsMono-9:weigth=regular:width=normal")
 				))
 
 ;; (push '(menu-bar-lines . 0)   default-frame-alist)
