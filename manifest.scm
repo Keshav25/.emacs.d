e4a0df74e29a9deb77b4206afd8e4e44976b19cb
Keshav25 <keshavitalia0@gmail.com>
2025-02-13 02:21:11 -0500

    working manifest.scm

--- manifest.scm
+++ manifest.scm
@@ -1,6 +1,26 @@
-(specifications->manifest '("git"
-							"cmake"
-							"emacs-4me"
-							"ripgrep"
-							"picom"
-							"git-delta"))
+(use-modules (guix profiles)
+			 (guix packages)
+			 (guix transformations)
+			 (gnu packages emacs)
+			 (gnu packages xorg)
+			 (gnu packages version-control)
+			 (gnu packages cmake)
+			 (gnu packages rust-apps)
+			 (gnu packages compton))
+
+(define-public k-emacs
+  (package
+   (inherit emacs-next)
+   (name "k-emacs")
+   (inputs (modify-inputs (package-inputs emacs-next)
+						  (prepend
+						   libxrender
+						   libxt)))))
+
+(packages->manifest (list
+					 git
+					  cmake
+					  k-emacs
+					  ripgrep
+					  picom
+					  git-delta))
