bcf1f9eb6daac1f5a91d88ceda753419e3c9d85f
Keshav25 <keshavitalia0@gmail.com>
2025-03-10 21:02:48 -0400

    Add: better settings for native-comp

--- early-init.el
+++ early-init.el
@@ -1,3 +1,8 @@
 (setq package-enable-at-startup nil
 	  package-native-compile t
+	  native-comp-always-compile t
+	  native-comp-async-jobs-number 12
+	  frame-inhibit-implied-resize t
+	  inhibit-startup-screen t
+	  warning-minimum-level :error
 	  byte-compile-ignore-files t)
