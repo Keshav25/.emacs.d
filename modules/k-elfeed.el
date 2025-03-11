81ad38985e9e547da274dc38273a562f0ef50200
Keshav25 <keshavitalia0@gmail.com>
2025-03-10 21:03:54 -0400

    Add: working on better elfeed workflow

--- modules/k-elfeed.el
+++ modules/k-elfeed.el
@@ -1,13 +1,38 @@
-(leaf elfeed :elpaca t
-  :custom
+;; -*- lexical-binding: t -*-
+
+(leaf elfeed
+  :elpaca t
   :bind
   (("C-c o r" . elfeed))
   (:elfeed-show-mode-map
    ("r" . elfeed-update))
+  (:elfeed-search-mode-map
+   ("n" . (elfeed-search-show-entry-pre +1))
+   ("p" . (elfeed-search-show-entry-pre -1))
+   ("M-RET" . (elfeed-search-show-entry-pre)))
   :config
   (load-file "~/.emacs.d/elfeed-feeds.el")
   (setq elfeed-db-directory "~/.emacs.d/elfeed/")
-  (setq elfeed-show-entry-switch 'display-buffer))
+
+  (defun browse-url-mpv (url &optional single)
+	(start-process "mpv" nil "mpv" (shell-quote-argument url)))
+
+  (setq elfeed-show-entry-switch 'switch-to-buffer-other-window)
+  (defun elfeed-display-buffer (buf &optional act)
+	(pop-to-buffer buf)
+	(set-window-text-height (get-buffer-window)
+							(round (* 0.7 (frame-height)))))
+
+  (defun elfeed-search-show-entry-pre (&optional lines)
+	"Returns a function to scroll forward or back in the Elfeed
+  search results, displaying entries without switching to them."
+    (lambda (times)
+      (interactive "p")
+      (forward-line (* times (or  lines 0)))
+      (recenter)
+      (call-interactively #'elfeed-search-show-entry)
+	  q      (select-window (previous-window))
+      (unless elfeed-search-remain-on-entry (forward-line -1)))))
 
 ;; Elfeed-Web
 ;; (leaf elfeed-web :after elfeed :elpaca t)
