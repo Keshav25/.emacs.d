deb9c5c82df78763c74f60e3d88d68707be57d87
Keshav25 <keshavitalia0@gmail.com>
2025-03-12 01:38:21 -0400

    Add: check if jetbrains mono exists before setting it

--- init.el
+++ init.el
@@ -158,8 +158,12 @@
 				(alpha . 100)
                 ;; (undecorated . t)
                 (vertical-scroll-bars . nil)
-                (horizontal-scroll-bars . nil)
-				(font . "JetBrainsMono-12:weigth=regular:width=normal")))
+                (horizontal-scroll-bars . nil)))
+
+(let ((f "JetbrainsMono"))
+  (when (member f (font-family-list))
+    (set-face-attribute 'default nil :font f)
+	(add-to-list 'default-frame-alist '(font . "JetBrainsMono-12:weigth=regular:width=normal"))))
 
 ;; (push '(menu-bar-lines . 0)   default-frame-alist)
 ;; (push '(tool-bar-lines . 0)   default-frame-alist)
