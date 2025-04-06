a06bab1bad7fdb571cd8f5c4a7864f28a4d39207
Keshav25 <keshavitalia0@gmail.com>
2025-04-06 15:35:06 -0400

    perf: usual gc hook

--- early-init.el
+++ early-init.el
@@ -1,3 +1,11 @@
+;; Temporarily increase GC threshold during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Restore to normal value after startup (e.g. 50MB)
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
+
+
 ;; User Information
 (setq user-full-name "Keshav Italia"
 	  user-mail-address "keshavitalia0@gmail.com")
