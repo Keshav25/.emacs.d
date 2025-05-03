62be984c0899f840bfa8cd27ea1fc4c72ed76b11
Keshav25 <keshavitalia0@gmail.com>
2025-05-02 20:15:20 -0400

    add: LSP_USE_PLISTS added to shell vars

--- modules/k-term.el
+++ modules/k-term.el
@@ -3,6 +3,8 @@
 (leaf exec-path-from-shell
   :when (not iswindows)
   :elpaca t
+  :custom
+  (exec-path-from-shell-variables . '("PATH" "MANPATH" "LSP_USE_PLISTS"))
   :config
   (exec-path-from-shell-initialize))
 
