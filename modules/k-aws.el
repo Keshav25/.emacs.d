452e92bb83ac9795beed1f649f213f38ab95fb26
Keshav25 <keshavitalia0@gmail.com>
2025-10-22 14:22:23 -0400

    chore: remove requiring aws

--- modules/k-aws.el
+++ modules/k-aws.el
@@ -2,7 +2,6 @@
   :doc "requires aws cli"
   :disabled t
   :elpaca (org-aws-iam-role :host github :repo "will-abb/org-aws-iam-role")
-  :require t
   :custom
   (org-aws-iam-role-profile . "my-profile") ;; Use a specific AWS CLI profile
   (org-aws-iam-role-read-only-by-default . t) ;; Open buffers in read-only mode
