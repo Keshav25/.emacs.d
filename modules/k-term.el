1bd1504b2c8b1e1f8fd94c5c747d763a12231dbc
Keshav25 <keshavitalia0@gmail.com>
2025-12-23 23:00:25 -0500

    add: more env vars in exec-path-from-shell-initiate

--- modules/k-term.el
+++ modules/k-term.el
@@ -6,7 +6,13 @@
   :when (not iswindows)
   :elpaca t
   :custom
-  (exec-path-from-shell-variables . '("PATH" "MANPATH" "LSP_USE_PLISTS"))
+  (exec-path-from-shell-variables . '("PATH"
+									  "MANPATH"
+									  "LSP_USE_PLISTS"
+									  "GOROOT"
+									  "GOPATH"
+									  "OLLAMA_BASE_URL"
+									  "OLLAMA_API_BASE"))
   :config
   (exec-path-from-shell-initialize))
 
