71dc7f5ed8d8897617acfe44a7cd46fb7741cd2a
Keshav25 <keshavitalia0@gmail.com>
2026-02-15 10:06:52 -0500

    change: do not enable global-blamer-mode by default

--- modules/k-vc.el
+++ modules/k-vc.el
@@ -259,7 +259,8 @@ for the \"main\" or \"master\" branch."
   :custom
   (blamer-force-truncate-long-line . nil)
   :config
-  (global-blamer-mode 1))
+  ;; (global-blamer-mode 1)
+  )
 
 (leaf syncthing
   :disabled t
